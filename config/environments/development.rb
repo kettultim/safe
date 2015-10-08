604818d94792c8117cfd225228dcd53e8c8854c7
pjskeptic <psparrow25@gmail.com>
2015-10-08 12:14:30 -0400

    s3 config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -44,7 +44,7 @@ Rails.application.configure do
   config.paperclip_defaults = {
     :storage => :s3,
     :s3_credentials => {
-      :bucket => 'safe-development',
+      :bucket => ENV['AWS_S3_BUCKET_NAME'],
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
     }
