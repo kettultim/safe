60d66a25195c970332a5670857c5e31c1c19f13e
pjskeptic <psparrow25@gmail.com>
2015-10-08 13:28:44 -0400

    Sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,6 +77,8 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  config.action_mailer.default_url_options = { host: 'safeapp.heroku.com' }
+
   config.paperclip_defaults = {
     :storage => :s3,
     :s3_credentials => {
@@ -86,3 +88,13 @@ Rails.application.configure do
     }
   }
 end
+
+ActionMailer::Base.smtp_settings = {
+  :address        => 'smtp.sendgrid.net',
+  :port           => '587',
+  :authentication => :plain,
+  :user_name      => ENV['SENDGRID_USERNAME'],
+  :password       => ENV['SENDGRID_PASSWORD'],
+  :domain         => 'heroku.com',
+  :enable_starttls_auto => true
+}
